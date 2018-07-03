e74b47f4f287b4384c9e3ac34e935b786c060928
Sergey Oganezov <oganez09@mail.ru>
2018-07-04 01:41:56 +0300

    edit

--- hochudom/js/custom.js
+++ hochudom/js/custom.js
@@ -1,11 +1,48 @@
 $(document).ready(function(){
 
-	$(".owl-carousel").owlCarousel({
-		items:1,
-		loop:false,
-		margin:10,
-		nav:true,
-		autoHeight:true,
+	$(".owl-carousel.owl-slide-big").owlCarousel({
+		margin: 30,
+		dots: false,
+		nav: true,
+		center: true,
+		navText: ["<img src='img/i_ar_left.png' class='owl_img'>", "<img src='img/i_ar_right.png' class='owl_img'>"],
+		responsive: {
+			0: {
+				items: 1
+			},
+			450: {
+				items: 2
+			},
+			650: {
+				items: 3
+			}
+		}
+	});
+
+	$(".owl-carousel.owl-slide-small").owlCarousel({
+		margin: 10,
+		dots: false,
+		center: true,
+		responsive: {
+			0: {
+				items: 3
+			},
+			450: {
+				items: 6
+			},
+			650: {
+				items: 10
+			}
+		}
+	});
+
+	$(".owl-carousel.owl-reviews").owlCarousel({
+		items: 1,
+		loop: false,
+		margin: 10,
+		nav: true,
+		dots: false,
+		autoHeight: true,
 		navText: ["<img src='img/owl_ar_left.png' class='owl_img'>", "<img src='img/owl_ar_right.png' class='owl_img'>"]
 	})
 	.on('changed.owl.carousel', function(event) {
@@ -29,10 +66,10 @@ $(document).ready(function(){
 		
 		var target = $(this).attr("href");
 		
-		$("[data-toggle = us-tab]").parent().removeClass("active");
+		$(this).closest(".us_tab_nav").children().removeClass("active");
 		$(this).parent().addClass("active");
 
-		$(".us_tab_pane").removeClass("active");
+		$(target).parent().children(".us_tab_pane").removeClass("active");
 		$(target).addClass("active");
 	})
 
