918b333ffcbe1251f36013c38a83187b5a0a507d
Sergey Oganezov <oganez09@mail.ru>
2018-10-15 20:19:38 +0300

    rost: delete

--- rost/ajax.php
+++
@@ -1,9 +0,0 @@
-<?php
-	
-	$email = "test@mail.ru";
-	$theme = "Персональная скидка";
-	$message = "Имя: ".$_POST['name'].", Телефон: ".$_POST['phone'].", email: ".$_POST['email'];
-
-	mail($email, $theme, $message);
-
-?>
