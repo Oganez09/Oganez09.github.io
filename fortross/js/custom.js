2d69fd375fa27fa65269f077b92f9db0ac20ee1d
Sergey Oganezov <oganez09@mail.ru>
2018-11-20 19:58:57 +0300

    fortross: product gallery

--- fortross/js/custom.js
+++ fortross/js/custom.js
@@ -8,6 +8,37 @@ $(document).ready(function() {
 		smartSpeed: 500
 	});
 	
+	$(".product_gallery").owlCarousel({
+		items: 1,
+		smartSpeed: 500,
+		responsive: {
+			0: {
+				dots: true
+			},
+			992: {
+				dots: false
+			}
+		}
+	});
+
+	$('.product_thrumb').slick({
+		infinite: false,
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		nav: true,
+		vertical: true,
+		verticalSwiping: true,
+		prevArrow: '<button class="slick_prev"><i class="icon-up-big"></i></button>',
+		nextArrow: '<button class="slick_next"><i class="icon-down-big"></i></button>'
+	});
+
+	$(".slick-slide").on("click", function() {
+		var index = $(this).index();
+
+		$(".product_gallery").trigger("to.owl.carousel", [index, 500, true]);
+	});
+	
+	
 	$("#open-menu").on("click", function() {
 		var target = $(this).attr("data-target");
 		if($(target).hasClass("show")) {
