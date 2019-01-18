a907507d14684455f6c787fa53711b5596d3a5f7
Sergey Oganezov <oganez09@mail.ru>
2019-01-18 20:59:57 +0300

    abc efg: home page end

--- abc_efg/js/custom.js
+++ abc_efg/js/custom.js
@@ -1,3 +1,19 @@
+$(document).ready(function() {
+
+	$(".num_icon").hover(function() {
+		$(this).toggleClass("show");
+	})
+
+	$(".us_flex_name").on("click", function() {
+		if ($(this).hasClass("us_flex_name_show")) {
+			$(this).removeClass("us_flex_name_show").parent().children(".us_flex_col:not(.us_flex_name)").slideUp(300);
+		}
+		else {
+			$(this).addClass("us_flex_name_show").parent().children(".us_flex_col").slideDown(300);
+		}		
+	})
+
+});
 
 $(document)
 .on("click", "#open_menu", function() {
