4ba51c289c33959632873c6ed68e4471457addc6
Sergey Oganezov <oganez09@mail.ru>
2019-01-19 10:22:31 +0300

    abc efg: home page edit bg btn and other

--- abc_efg/js/custom.js
+++ abc_efg/js/custom.js
@@ -4,14 +4,16 @@ $(document).ready(function() {
 		$(this).toggleClass("show");
 	})
 
-	$(".us_flex_name").on("click", function() {
-		if ($(this).hasClass("us_flex_name_show")) {
-			$(this).removeClass("us_flex_name_show").parent().children(".us_flex_col:not(.us_flex_name)").slideUp(300);
-		}
-		else {
-			$(this).addClass("us_flex_name_show").parent().children(".us_flex_col").slideDown(300);
-		}		
-	})
+	if ($(window).width() < "1199") {
+		$(".us_flex_name").on("click", function() {
+			if ($(this).hasClass("us_flex_name_show")) {
+				$(this).removeClass("us_flex_name_show").parent().children(".us_flex_col:not(.us_flex_name)").slideUp(300);
+			}
+			else {
+				$(this).addClass("us_flex_name_show").parent().children(".us_flex_col").slideDown(300);
+			}		
+		});
+	}
 
 });
 
@@ -22,10 +24,12 @@ $(document)
 		$(target).removeClass("open");
 	} else {
 		$(target).addClass("open");
+		$("body").append("<div id='offcanvas_overlay'></div>");
 	}
 })
-.on("click", "#offcanvas_close", function() {
+.on("click", "#offcanvas_close, #offcanvas_overlay", function() {
 	$("#offcanvas").removeClass("open");
+	$("#offcanvas_overlay").remove();
 });
 
 $(window).on("load resize", windowSize);
@@ -34,5 +38,6 @@ function windowSize() {
 	if ($(window).width() > "991") {
 		var menu = $("#open_menu").attr("data-target");
 		$(menu).removeClass("open");
+		$("#offcanvas_overlay").remove();
 	}
 }
