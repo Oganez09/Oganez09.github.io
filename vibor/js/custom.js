8773483843bbc68d0520b7bae9f4214c85c20ed4
Oganez09 <oganez09@mail.ru>
2019-07-15 12:45:55 +0300

    add vibor

---
+++ vibor/js/custom.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+	$(document)
+	.on("click", "[data-toggle = us_modal]", function(e) {
+		e.preventDefault();
+
+		if ($(this).is("a")) {
+			var target = $(this).attr("href");
+		} else if($(this).is("button")) {
+			var target = $(this).attr("data-target");
+		}
+
+		$(target).addClass("us_visible");
+	})
+	.on("click", "[data-dismiss = us_modal], .us_modal", function() {
+		$(".us_modal").removeClass("us_visible");
+	})
+	.on("click", ".us_modal_box", function(e) {
+		e.stopPropagation();
+	})
+
+	.on("click", "[data-toggle = us_tabs]", function(e) {
+		e.preventDefault();
+
+		var target = $(this).attr("href");
+
+		$(this).closest(".us_tabs_nav").children().removeClass("active");
+		$(this).parent().addClass("active");
+
+		$(this).closest(".us_tabs_nav").next().children().removeClass("active");
+		$(target).addClass("active");
+	});
+
+	$(window).on("scroll load", function () {
+		if ($(this).scrollTop() > 1) {
+			$(".home .header").addClass("header_fixed");
+		} else {
+			$(".home .header").removeClass("header_fixed");
+		}
+	});
+
+});
