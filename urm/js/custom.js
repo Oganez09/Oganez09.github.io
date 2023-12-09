bd2fc618fe1ef007760681c0192e6f958629dbb1
Sergey Oganezov <oganez09@mail.ru>
2023-12-09 13:34:54 +0300

    urm update 3

--- urm/js/custom.js
+++ urm/js/custom.js
@@ -1,25 +1,36 @@
 $(document).ready(function () {
   $(document)
     .on('click', '.urm_card_head', function () {
-      $(this).next().slideToggle(300).parent().toggleClass('show');
+      // $(this).next().slideToggle(300).parent().toggleClass('show');
+      if ($(this).parent().hasClass('show')) {
+        $(this).next().slideUp(300).parent().removeClass('show');
+      } else {
+        $(this).next().slideDown(300).parent().addClass('show');
+      }
     })
     .on('click', '.urm_card_toggle_all', function () {
-      $(this).toggleClass('show_all');
-      $('.urm_card').find('.urm_card_body').slideToggle(300);
+      // $(this).toggleClass('show_all');
+      // $('.urm_card').find('.urm_card_body').slideToggle(300);
       if ($(this).hasClass('show_all')) {
-        $(this).text("Развернуть все");
-      } else {
         $(this).text("Свернуть все");
+        $(this).removeClass('show_all');
+        $('.urm_card').addClass('show');
+        $('.urm_card').find('.urm_card_body').slideDown(300);
+      } else {
+        $(this).text("Развернуть все");
+        $(this).addClass('show_all');
+        $('.urm_card').removeClass('show');
+        $('.urm_card').find('.urm_card_body').slideUp(300);
       }
     });
 });
 
 function init () {
-  var multiRoute = new ymaps.multiRouter.MultiRoute({
-      referencePoints: [
-          [62.998683, 76.949073],
-          [65.568653, 72.372517],
-      ],
+  const a = [62.998683, 76.949073];
+  const b = [65.568653, 72.372517];
+
+  const multiRoute = new ymaps.multiRouter.MultiRoute({
+      referencePoints: [ a, b ],
       params: {
           results: 1
       }
@@ -27,8 +38,8 @@ function init () {
       boundsAutoApply: true
   });
 
-  var myMap = new ymaps.Map('map', {
-      center: [55.750625, 37.626],
+  const myMap = new ymaps.Map('map', {
+      center: [64, 74],
       zoom: 7,
       controls: []
   });
