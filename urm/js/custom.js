53c576f73162ae0923f2b0e8859098608f191818
Sergey Oganezov <oganez09@mail.ru>
2023-12-09 16:08:41 +0300

    urm update 6 map

--- urm/js/custom.js
+++ urm/js/custom.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
   $(document)
     .on('click', '.urm_card_head', function () {
-      // $(this).next().slideToggle(300).parent().toggleClass('show');
       if ($(this).parent().hasClass('show')) {
         $(this).next().slideUp(300).parent().removeClass('show');
       } else {
@@ -9,8 +8,6 @@ $(document).ready(function () {
       }
     })
     .on('click', '.urm_card_toggle_all', function () {
-      // $(this).toggleClass('show_all');
-      // $('.urm_card').find('.urm_card_body').slideToggle(300);
       if ($(this).hasClass('show_all')) {
         $(this).text("Свернуть все");
         $(this).removeClass('show_all');
@@ -26,25 +23,35 @@ $(document).ready(function () {
 });
 
 function init () {
-  const a = [62.998683, 76.949073];
-  const b = [65.568653, 72.372517];
+  const startPoint = [62.998683, 76.949073];
+  const endPoint = [65.568653, 72.372517];
 
   const multiRoute = new ymaps.multiRouter.MultiRoute({
-      referencePoints: [ a, b ],
-      params: {
-          results: 1
-      }
+    referencePoints: [ startPoint, endPoint ],
+    params: {
+      results: 1
+    }
   }, {
-      boundsAutoApply: true
+    wayPointStartIconLayout: "default#image",
+    wayPointStartIconImageHref: "img/startPoint.png",
+    wayPointStartIconImageSize: [16, 20],
+    wayPointStartIconImageOffset: [-8, -10],
+    wayPointFinishIconLayout: "default#image",
+    wayPointFinishIconImageHref: "img/endPoint.png",
+    wayPointFinishIconImageSize: [16, 20],
+    wayPointFinishIconImageOffset: [-8, -10],
+
+    routeActiveStrokeColor: "#0a39c2",
+
+    boundsAutoApply: true,
   });
 
   const myMap = new ymaps.Map('map', {
-      center: [64, 74],
-      zoom: 7,
-      controls: []
+    center: [64, 74],
+    zoom: 7,
+    controls: []
   });
 
-  // Добавляем мультимаршрут на карту.
   myMap.geoObjects.add(multiRoute);
 }
 
