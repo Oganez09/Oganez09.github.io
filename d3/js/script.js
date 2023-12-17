03bf6e907cdbf7d7983d16d1cc68bd30c24ff111
Sergey Oganezov <oganez09@mail.ru>
2023-12-17 06:59:12 +0300

    d3 update 1

--- d3/js/script.js
+++ d3/js/script.js
@@ -4,3 +4,15 @@ $returnBack.addEventListener('click', () => {
   history.back();
 });
 
+const get = window.location.search.replace( '?', '');
+const arr = get.split('&').reduce(
+  function(p,e){
+      var a = e.split('=');
+      p[ decodeURIComponent(a[0])] = decodeURIComponent(a[1]);
+      return p;
+  },
+  {}
+);
+console.log(arr)
+
+fetch(get);
