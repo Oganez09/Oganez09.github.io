908f03321b26d2eff92169e3a32627ca3df97e5f
Sergey Oganezov <oganez09@mail.ru>
2023-12-17 20:31:36 +0300

    d3 end

--- d3/js/script.js
+++ d3/js/script.js
@@ -15,6 +15,6 @@ const get = window.location.search
   //   return obj;
   // }, {});
 
-const sendMsg = `https://api.telegram.org/bot6404900072:AAEWApiLlg1mAHquxDcJ9oVwrfzI7Fx91HY/sendMessage?chat_id=-1002114761565&text=${get}`;
+// const sendMsg = `https://api.telegram.org/bot6404900072:AAEWApiLlg1mAHquxDcJ9oVwrfzI7Fx91HY/sendMessage?chat_id=-1002114761565&text=${get}`;
 
-fetch(sendMsg);
+// fetch(sendMsg);
